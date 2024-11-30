5ceea3c0a918b4ffc0802a335b75039021470774
techaura <noskoffv@gmail.com>
2024-11-30 18:00:45 +0200

    initial commit
    First fill

---
+++ build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/dev/esp/v5.3/esp-idf/components/bootloader/subproject"
+  "C:/dev/esp/c3_wifi/wifista/build/bootloader"
+  "C:/dev/esp/c3_wifi/wifista/build/bootloader-prefix"
+  "C:/dev/esp/c3_wifi/wifista/build/bootloader-prefix/tmp"
+  "C:/dev/esp/c3_wifi/wifista/build/bootloader-prefix/src/bootloader-stamp"
+  "C:/dev/esp/c3_wifi/wifista/build/bootloader-prefix/src"
+  "C:/dev/esp/c3_wifi/wifista/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/dev/esp/c3_wifi/wifista/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/dev/esp/c3_wifi/wifista/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
